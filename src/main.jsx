d9acd7739146c03487c950a90c3841c537ef2c43
Nicolas Menezes <nicolas.seabra09@gmail.com>
2026-08-20 00:13:59 -0300

    Apply Sender Pro branding assets

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App, { ErrorBoundary } from './App'
 import './styles.css'
+import './brand.css'
 
 createRoot(document.getElementById('root')).render(
   <ErrorBoundary>
